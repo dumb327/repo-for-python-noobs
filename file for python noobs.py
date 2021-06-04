8f85d73228ad21663c4efe3922f7cee0a585d75f
dumb327 <85287901+dumb327@users.noreply.github.com>
2021-06-04 23:51:45 +0530

    Create file for python noobs.py

---
+++ file for python noobs.py
@@ -0,0 +1,11 @@
+age = 0
+age += 0
+print(age)
+
+first_name = "your name "
+last_name = "your last name"
+full_name = first_name + last_name
+print("Hello " + full_name)
+
+#this is it. copy paste it , save it as a .py file , something like python-file.py and run this. 
+#change the number 0 in age to any random number , change "your_name and "last_name" to your name / any other name.
